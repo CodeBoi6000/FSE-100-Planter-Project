47bc4fef64640c156732aae1356bceebbf3b9afa
Jacob Brenner <65000323+CodeBoi6000@users.noreply.github.com>
2025-09-10 21:23:42 -0700

    added PWM motor control

--- PlanterCode/PlanterCode.ino
+++ PlanterCode/PlanterCode.ino
@@ -1,7 +1,7 @@
 #include <dht.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 13, 5, 4, 3, 2);
 dht DHT;
 
 #define DHT11_PIN 7
@@ -10,7 +10,7 @@ dht DHT;
 int redPin= 10;
 int greenPin = 9;
 int bluePin = 8;
-int fanPin = 13;
+int fanPin = 11;
 int BUT_PIN = 6;
 
 void setup(){
@@ -22,36 +22,61 @@ void setup(){
   pinMode(bluePin, OUTPUT);
 }
 
-class UI(){
-  void setColor(int redValue, int greenValue,  int blueValue) {
-    analogWrite(redPin, redValue);
-    analogWrite(greenPin,  greenValue);
-    analogWrite(bluePin, blueValue);
-  }
+class UI{
+  public:
+    void setColor(int redValue, int greenValue,  int blueValue) {
+      analogWrite(redPin, redValue);
+      analogWrite(greenPin,  greenValue);
+      analogWrite(bluePin, blueValue);
+    }
 
   double getJoystick(int axis){
     if (axis == 0) {
-      return(digitalRead(BUT_PIN))
+      return(digitalRead(BUT_PIN));
     } else if (axis == 1) {
-      return(double(analogRead(VRX_PIN)-504)/503.00)
+      return(double(analogRead(VRX_PIN)-504)/503.00);
     } else if (axis == 2){
-      return(double(analogRead(VRY_PIN)-518)/505.00)
-    } else {
-      break;
+      return(double(analogRead(VRY_PIN)-518)/505.00);
     }
   }
-}
+};
 
-class fan() {
-  void run(double speed) {
-    analogWrite(fanPin, 255 * speed);
-  
-  void stop() {
-    analogWrite(finPin, 0);
-  }
-}
+class fan {
+  public:
+    void run(double speed) {
+      analogWrite(fanPin, 255 * speed);
+    }
+
+  public:
+    void stop() {
+      analogWrite(fanPin, 0);
+    }
+};
+
+class tempPID {
+  private:
+    double ref = 0;
+  public:
+    void setPID(double P, double I, double D){
+      double tP = P;
+      double tI = I;
+      double tD = D;
+    }
+
+    void setRef(double Target){
+      ref = Target;
+      double accErr = 0;
+    }
+
+    void getResult(double Input) {
+      double error =  - Input;
+    }
+};
 
 void loop(){
+  int chk = DHT.read11(DHT11_PIN);
+  UI UI;
+  fan fan;
   lcd.setCursor(0,0); 
   lcd.print("Temp: ");
   lcd.print(DHT.temperature);
@@ -61,13 +86,14 @@ void loop(){
   lcd.print("Humidity: ");
   lcd.print(DHT.humidity);
   lcd.print("%");
-  if (DHT.humidity>50 && DHT.humidity<70){
+  if (DHT.humidity>65 && DHT.humidity<70){
     UI.setColor(255,50,0);
+    fan.run(0.5);
   } else if (DHT.humidity>69) {
     UI.setColor(255,0,0);
-    digitalWrite(fan, 1);
+    fan.run(1);
   } else {
     UI.setColor(0,255,0);
-    digitalWrite(fan, 0);
+    fan.stop();
   }
 }
